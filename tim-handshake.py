62ed999f8239910791f1c681f723d7ffcbad217c
Phill Conrad <pconrad@cs.ucsb.edu>
2016-05-25 09:31:04 -0700

    Update tim-handshake.py

--- tim-handshake.py
+++ tim-handshake.py
@@ -1,11 +1,13 @@
 # oscOut.py
 
 from osc import *
+from music import *        # import music library
 
 def head(message):      
   print "Got /1/vel/head"
   args = message.getArguments()  # get OSC message's arguments
   print "args=",str(args)
+  Play.midi(note)            # and play it!
 
 def handshakeWithTim():
   oscOut = OscOut("169.231.139.109", 8001) 
@@ -14,5 +16,6 @@ def handshakeWithTim():
 if __name__=="__main__":
     handshakeWithTim()
     oscIn = OscIn( 8000 ) 
-    oscIn.onInput("/1/vel/head", simple)
-  
+    oscIn.onInput("/1/vel/head", head)
+    note = Note(C4, HN)        # create a middle C half note
+
