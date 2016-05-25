001daae39306daa5fb76f7142c90f5ed715a6eae
Phill Conrad <pconrad@cs.ucsb.edu>
2016-05-25 09:28:32 -0700

    Update tim-handshake.py

--- tim-handshake.py
+++ tim-handshake.py
@@ -2,6 +2,17 @@
 
 from osc import *
 
-oscOut = OscOut("169.231.139.109", 8001) 
+def head(message):      
+  print "Got /1/vel/head"
+  args = message.getArguments()  # get OSC message's arguments
+  print "args=",str(args)
 
-oscOut.sendMessage("/handshake")
+def handshakeWithTim():
+  oscOut = OscOut("169.231.139.109", 8001) 
+  oscOut.sendMessage("/handshake")
+
+if __name__=="__main__":
+    handshakeWithTim()
+    oscIn = OscIn( 8000 ) 
+    oscIn.onInput("/1/vel/head", simple)
+  
